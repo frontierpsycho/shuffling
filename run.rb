8c3e87a753164a2b60d75c87db369f5ca3599c54
frontierpsycho <unfortunate42@gmail.com>
2014-04-30 13:48:58 +0200

    Use named arguments.

--- run.rb
+++ run.rb
@@ -2,7 +2,7 @@ require './shuffling'
 require './shufflers'
 
 module Shuffling
-  evaluator = Evaluator.new(520_000, 52)
+  evaluator = Evaluator.new(deck_size: 52, times: 520_000)
 
   evaluator.evaluate(RubyShuffler.new)
   puts evaluator.results
