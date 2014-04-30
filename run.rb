62a2aa898046b689d19da84442bd6a87926793d1
frontierpsycho <unfortunate42@gmail.com>
2014-04-30 16:23:59 +0200

    Introduced celluloid and actor structure.

--- run.rb
+++ run.rb
@@ -1,12 +1,64 @@
+require 'celluloid'
+
 require './shuffling'
 require './shufflers'
 
-module Shuffling
-  evaluator = Evaluator.new(deck_size: 30, times: 300_000)
 
-  evaluator.evaluate(RubyShuffler.new)
-  evaluator.evaluate(PileShuffle.new)
-  evaluator.evaluate(VariablePileShuffle.new)
-  evaluator.evaluate(VariablePileShuffleHuman.new)
-  puts evaluator.results
+module Shuffling
+  class AggregatorActor
+    include Celluloid
+
+    def initialize(shufflers)
+      @shufflerset = shufflers
+      @results = {}
+    end
+
+    def add_result(result)
+      @results.merge! result
+
+      @shufflerset = @shufflerset - result.keys
+
+      puts "Results for #{result.keys.inspect} recorded, remaining: #{@shufflerset.inspect}"
+
+      if @shufflerset.empty?
+        self.output
+        self.terminate
+      end
+    end
+
+    def output
+      puts @results
+    end
+  end
+
+  class EvalActor
+    include Celluloid
+
+    def initialize(shufflerClass)
+      @shuffler = shufflerClass.new
+      self.runEvaluation
+    end
+
+    def runEvaluation
+      puts "Running #{@shuffler.name}"
+      evaluator = Evaluator.new
+      puts "#{@shuffler.name} evaluating..."
+      evaluator.evaluate(@shuffler)
+      puts "#{@shuffler.name} evaluated."
+      Celluloid::Actor[:aggregator].add_result(evaluator.results)
+      self.terminate
+    end
+  end
+
+  class ShufflerSupervisionGroup < Celluloid::SupervisionGroup
+    shufflers = [RubyShuffler, PileShuffle, VariablePileShuffle, VariablePileShuffleHuman].to_set
+
+    supervise AggregatorActor, as: :aggregator, args: [shufflers.map { |sh| sh.new.name }]
+
+    shufflers.each do |shuffler|
+      supervise EvalActor, as: shuffler.name.to_sym, args: [shuffler]
+    end
+  end
+
+  ShufflerSupervisionGroup.run!
 end
